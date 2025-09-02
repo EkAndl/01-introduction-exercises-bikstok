7b53e541b1c07669866538e80b491c2404778714
Bikstok <jkjaerulff@gmail.com>
2025-09-02 10:20:30 +0200

    Solved 2._Strings

--- 2._Strings.js
+++ 2._Strings.js
@@ -7,6 +7,9 @@ const numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
+const resultStringToFloat = parseFloat(numberOne) + parseFloat(numberTwo);
+
+console.log(resultStringToFloat);
 
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
@@ -14,6 +17,9 @@ const numberTwo = "2.30";
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
+const resultWith2Decimals = (parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo)).toFixed(2);
+
+console.log(resultWith2Decimals);
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -24,14 +30,21 @@ const three = 98;
 
 // Show in the console the avg. with 5 decimals
 
+const averageWith5Decimals = ((one + two + three) / 3).toFixed(5);
 
+console.log(averageWith5Decimals);
 
 
 // --------------------------------------
 // Exercise 6 - Get the character by index
 
 const letters = "abc";
+
 // Get me the character "c"
+
+const characterC = letters.charAt(2);
+
+console.log(characterC);
 
 
 
@@ -43,7 +56,9 @@ const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
 
+const capitalizedJ = fact.replace("javascript", "Javascript");
 
+console.log(capitalizedJ);
 // --------------------------------------
 
 
